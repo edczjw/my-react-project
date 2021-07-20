41630c1b39e9f15629f1b4b1780fa8712b0d0473
zengjunwei <347350971@qq.com>
2021-07-20 14:23:41 +0800

    fix: 测试

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ export default {
       apiBaseUrl: '/api' // 开发环境接口请求，后用于 proxy 代理配置
     }, 
     production: {
-      cdn: '//s.xxx.com/vite-react-app/prd', // 正式环境 cdn 路径
+      cdn: 'allenboy.cn/', // 正式环境 cdn 路径
       apiBaseUrl: '//www.xxx.com/v1' // 正式环境接口地址
     }
   }
