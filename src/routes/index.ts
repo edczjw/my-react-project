790c74a2b2d82b4741472a5e18bf7e129cd2513b
zengjunwei <347350971@qq.com>
2021-07-21 11:14:33 +0800

    fix: 会计核算地方

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import Ahome from '@/view/Home';
-import Mypage from '@/view/Mypage';
-import Login from '@/view/Login';
+// import Mypage from '@/view/Mypage';
+// import Login from '@/view/Login';
 
 import {
     YahooOutlined, AlibabaOutlined, YuqueOutlined, LaptopOutlined, UngroupOutlined, UsergroupAddOutlined, PropertySafetyOutlined, IdcardOutlined, ReadOutlined, FunctionOutlined, ToolOutlined, MailOutlined, AppstoreOutlined, SettingOutlined
@@ -19,14 +19,14 @@ const routes = [
         id:'Mypage',
         path: '/Mypage',
         title: '我的',
-        component: ()=>{import('@/view/Mypage')},
+        component: Ahome,
         icon: IdcardOutlined,
         childrens:{}
     },{
         id:'Login',
         path: '/Login',
         title: '登录',
-        component: ()=>{import('@/view/Login')},
+        component: Ahome,
         icon: FunctionOutlined,
         childrens:{}
     }
